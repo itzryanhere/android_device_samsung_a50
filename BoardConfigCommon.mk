cd56ab0fddaf62bf8f472c45cbb6bdadaa82cf22
GiovanYCringe <gio.pikagrossa@gmail.com>
2023-04-08 01:33:48 +0200

    Use clang clang drop debug-tools

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -88,10 +88,8 @@ BOARD_KERNEL_IMAGE_NAME := Image
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/samsung/a50
-TARGET_KERNEL_LLVM_BINUTILS := false
 TARGET_KERNEL_CONFIG := a50_defconfig
-TARGET_KERNEL_CLANG_VERSION := proton
-TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/proton-clang
+TARGET_KERNEL_CLANG_VERSION := r383902
 
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
